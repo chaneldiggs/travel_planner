890d567992d2ab37b9ce72c8acfd7cc2027e12f2
chaneldiggs <chaneldiggs@gmail.com>
2016-11-02 23:55:18 -0400

    added filler data to trips show page

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,10 @@ class Trip < ApplicationRecord
 	belongs_to :user
 	
 	mount_uploader :image, ImageUploader
+
+	validates :name, presence: true, uniqueness: true
+	validates :dates, presence: true
+  validates :return_date, presence: true
+  validates :city, presence: true
+  
 end
